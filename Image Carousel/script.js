421898a297d65c5b1f308350e45c3febdd824bb5
Sriramjai Anjamani <sriramjai_a@hotmail.com>
2021-02-06 22:03:56 -0500

    Add carousel effect to image container

---
+++ Image Carousel/script.js
@@ -0,0 +1,28 @@
+const imageContainer = document.getElementById('imgs');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+
+const img = document.querySelectorAll('#imgs img');
+
+let idx = 0;
+
+let interval = setInterval(run, 2000)
+
+
+function run(){
+    idx++;
+    
+    changeImage();
+}
+
+
+function changeImage() {
+     if(idx > img.length -1 ) {
+         idx = 0;
+     } else if (idx < 0) {
+         idx = img.length -1;
+     }
+
+
+    imageContainer.style.transform = `translateX(${-idx * 500}px)`;
+}
