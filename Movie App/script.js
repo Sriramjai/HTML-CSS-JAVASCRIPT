e53e95ca5dea8dbaea33d9a1221fbe0545e5e33d
Sriramjai Anjamani <sriramjai_a@hotmail.com>
2021-02-13 20:16:45 -0500

    Add Utility class getClassByRate to get vote ratings

--- Movie App/script.js
+++ Movie App/script.js
@@ -21,7 +21,7 @@ function showMovies(movies) {
     main.innerHTML = '';
 
     movies.forEach((movie) => {
-        const { title, poster_path, vote, overview} = movie;
+        const { title, poster_path, vote_average, overview} = movie;
 
         const movieEl = document.createElement('div');
         movieEl.classList.add('movie');
@@ -29,21 +29,34 @@ function showMovies(movies) {
         movieEl.innerHTML = `
         
         <div class="movie">
-            <img src="` + movie.poster_path + `" alt="movie">
+            <img src="${IMG_PATH + poster_path }" alt="movie">
             <div class="movie-info">
-                <h3>` + movie.title + `</h3>
-                <span class="green"> ` + movie.vote + `</span>
+                <h3>${title}</h3>
+                <span class="${getClassByRate(vote_average)}"> ${vote_average}</span>
             </div>
             <div class="overview">
-                <h3>Overview</h3> ` + movie.overview + `
+                <h3>Overview</h3> ${overview}
                 
             </div>
         </div>
         
         `;
+
+        main.appendChild(movieEl);
     })
 }
 
+
+function getClassByRate(vote) {
+    if (vote >= 8 ) {
+        return "green";
+    } else if (vote >= 5 < 8) {
+        return "orange";
+    } else {
+        return "red";
+    }
+
+}
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
